25c29eb41884ed8136df39e5230ddef282c548cd
MarcosMora <abreu760@hotmail.com>
2025-09-07 00:13:35 -0400

    updates json

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,27 +1,52 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
-    # solicitud POST
+    # POST
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    payload = { "raw_document": { "text": text_to_analyze } }
+    payload = {"raw_document": {"text": text_to_analyze}}
     
     try:
-        #  POST
-        response = requests.post(url, headers=headers, json=payload, timeout=10)
-        response.raise_for_status()  # Lanza una excepción para códigos de estado HTTP no exitosos
         
-        # código de estado
+        response = requests.post(url, headers=headers, json=payload)
+        
         if response.status_code == 200:
-            return response.text  # respuesta
+            
+            response_dict = json.loads(response.text)
+            
+            emotions = response_dict['emotionPredictions'][0]['emotion']
+            
+            
+            anger_score = emotions['anger']
+            disgust_score = emotions['disgust']
+            fear_score = emotions['fear']
+            joy_score = emotions['joy']
+            sadness_score = emotions['sadness']
+            
+            
+            emotion_scores = {
+                'anger': anger_score,
+                'disgust': disgust_score,
+                'fear': fear_score,
+                'joy': joy_score,
+                'sadness': sadness_score
+            }
+            
+            dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+            
+            return {
+                'anger': anger_score,
+                'disgust': disgust_score,
+                'fear': fear_score,
+                'joy': joy_score,
+                'sadness': sadness_score,
+                'dominant_emotion': dominant_emotion
+            }
         else:
-            return f"Error: Received status code {response.status_code}. Response: {response.text}"
+            
+            return f"Error: Received status code {response.status_code}"
     
     except requests.exceptions.RequestException as e:
+        
         return f"An error occurred: {str(e)}"
-
-# Para probar la función 
-if __name__ == "__main__":
-    # Prueba co
-    result = emotion_detector("Me encanta esta nueva tecnología")
-    print(result)
